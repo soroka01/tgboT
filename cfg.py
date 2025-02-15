4b07fcaf17cc1584a3f1408bac82fe782efc87b0
Timofey Soroka <48842014+soroka01@users.noreply.github.com>
2025-02-15 06:12:56 +0500

    Файл конфигурации

--- cfg.py
+++ cfg.py
@@ -1,6 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
 symb = "BTCUSDT"
 timeframe = 60
-tgtoken = 'YOUR_TELEGRAM_BOT_TOKEN'
-tgID = [YOUR_TELEGRAM_USER_ID]
-bykey = 'YOUR_BYBIT_API_KEY'
-bysecret = 'YOUR_BYBIT_API_SECRET'
+tgtoken = os.getenv('TG_TOKEN')
+tgID = [1111, 1111, 1111]
+bysecret = os.getenv('BY_SECRET', '')
+bykey = os.getenv('BY_KEY', '')
+
+# Переменные для RSI
+rsi_thresholds = {
+    '35': True,
+    '30': True,
+    '70': False,
+    '60': False
+}
+
+rsi_threshold_35 = 35
+rsi_threshold_30 = 30
+rsi_threshold_70 = 70
+rsi_threshold_60 = 60

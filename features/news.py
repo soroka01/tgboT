43e6ba888306f83ebc55225e5c583e612a3c26d6
Timofey Soroka <48842014+soroka01@users.noreply.github.com>
2025-02-15 06:20:13 +0500

    Модуль для получения новостей

--- features/news.py
+++ features/news.py
@@ -1,5 +1,5 @@
 from logs.logging_config import logging
-
+ 
 def get_latest_crypto_news():
     # Функция возвращает последние новости о криптовалютах
     logging.info("Получение последних новостей о криптовалютах")
